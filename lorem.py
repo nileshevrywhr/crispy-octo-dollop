4812bc699f69edf5fcdf537beddd496afc1f3151
nileshevrywhr <nileshevrywhr@debian-BULLSEYE-live-builder-AMD64>
2023-09-18 22:31:45 +0530

    adds fallback mechanism

--- lorem.py
+++ lorem.py
@@ -1,52 +1,50 @@
 from gtts import gTTS
 import pygame, pyttsx3
-import requests, json
+import requests, json, datetime, pytz
+from dateutil import parser
+from io import BytesIO
 
-AW_KEY = "PlYNYwY61E9c7YOJyoZfYDEFEGwywR71"
-LOCATION = "3146227"
+AW_KEY      = "PlYNYwY61E9c7YOJyoZfYDEFEGwywR71"
+LOCATION    = "3146227"
+url         = f"http://dataservice.accuweather.com/currentconditions/v1/{LOCATION}?apikey={AW_KEY}"
 
-url = f"http://dataservice.accuweather.com/currentconditions/v1/{LOCATION}?apikey={AW_KEY}"
+insightful_statement = f"Greetings! It's 7:00 PM, the weather in NOIDA is 31°, partly cloudy with low chances of precipitation."
 
-# Initialize the Pygame mixer
 pygame.mixer.init()
 
 response = requests.get(url)
-if response.ok:
+if response.status_code == 200:
     data = response.json()[0]
     with open("response.json", "w") as fw:
         json.dump(data, fw)
-    # Extract relevant data
-    observation_time = data["LocalObservationDateTime"]
     weather_text = data["WeatherText"]
     has_precipitation = data["HasPrecipitation"]
     temperature_celsius = data["Temperature"]["Metric"]["Value"]
 
-    # Create an insightful statement
+    timestamp = parser.parse(data["LocalObservationDateTime"])
+
+    observation_time = timestamp.strftime("%I:%M %p")
+
+    local_time = timestamp.astimezone(pytz.timezone('Asia/Kolkata'))
+
     if has_precipitation:
         precipitation_status = "with precipitation"
     else:
         precipitation_status = "without precipitation"
 
-    insightful_statement = f"At {observation_time}, the weather in Noida is {weather_text}, {precipitation_status}, and the temperature is {temperature_celsius}°C."
+    insightful_statement = f"Greetings! It's {observation_time}, the weather in NOIDA is {temperature_celsius}°, {weather_text} {precipitation_status}."
 
-    # Print the insightful statement
-    print(insightful_statement)
+else:
+    print(response.status_code)
 
+tts = gTTS(insightful_statement, lang='en', tld="co.in")
+tts.save("output.mp3")
 
-# text = "Hello, this is an example of text-to-speech synthesis."
-tts = gTTS(insightful_statement)
-tts.save("output.mp3")  # Save the speech to an audio file
-
-# Load the audio file
+print(insightful_statement)
 pygame.mixer.music.load("output.mp3")
-
-# Play the audio file
 pygame.mixer.music.play()
 
-# Block execution until the audio finishes playing
 while pygame.mixer.music.get_busy():
-    # pygame.time.Clock().tick(1)  # Adjust the tick rate as needed
     pass
 
-# Optionally, you can quit Pygame when done
 pygame.quit()
